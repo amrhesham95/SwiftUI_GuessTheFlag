5a5051d0876ccedafd0998f257cd132f186c0e2d
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2022-05-30 14:08:01 +0200

    Initial Commit

---
+++ SwiftUI_GuessTheFlag/SwiftUI_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GuessTheFlagApp.swift
+//  SwiftUI_GuessTheFlag
+//
+//  Created by Amr Hesham on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
